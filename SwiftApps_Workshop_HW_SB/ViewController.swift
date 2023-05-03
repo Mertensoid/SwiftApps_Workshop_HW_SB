e81a581168a4a0d1f8f5d27c364dc2fece2877b2
Mertensoid <matsoukm@yandex.ru>
2023-05-03 14:05:10 +0300

    #1 login screen + logo + label + 2x text fields + button

--- SwiftApps_Workshop_HW_SB/ViewController.swift
+++ SwiftApps_Workshop_HW_SB/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
     }
 
+    private func configureViews() {
+        loginTextField.layer.borderColor = UIColor.red.cgColor
+        loginTextField.layer.borderWidth = 1
+        loginTextField.layer.cornerRadius = 10
+        loginTextField.layer.masksToBounds = true
+        
+        passwordTextField.layer.borderColor = UIColor.red.cgColor
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.cornerRadius = 10
+        passwordTextField.layer.masksToBounds = true
+        
+        button.layer.cornerRadius = 10
+        button.layer.masksToBounds = true
+    }
 
 }
 
